bd5f872a51b316fbd75b77e60307aa54e9ca1a51
tmdals8030 <a7965344@gmail.com>
2021-03-26 12:40:41 +0900

    message

---
+++ 01/21.js
@@ -0,0 +1,11 @@
+function gugudan(){
+    for (front = 2; front <= 9; front++) {
+        for (back = 1; back <= 9; back++) 
+        {
+            console.log(front+"x"+back+"="+front*back)
+        }
+    }
+        
+}
+
+gugudan()
