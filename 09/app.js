0fd25f5eb6d6ae6c8cd095b49c064eaaad91a23a
tmdals8030 <48111893+tmdals8030@users.noreply.github.com>
2021-05-09 19:21:30 +0900

    09

---
+++ 09/app.js
@@ -0,0 +1,26 @@
+// /* eslint-disable no-console */
+// const promise = fetch("/json.json");
+// console.log(promise);
+
+// promise.then((res) => {
+//   console.log("res: ", res);
+//   const textPromise = res.text();
+//   console.log("textpromise: ", textPromise);
+
+//   textPromise.then((data) => console.log("data: ", data));
+// });
+
+// eslint-disable-next-line no-unused-vars
+function callback(data) {
+  // eslint-disable-next-line no-console
+  console.log("callback: ", data);
+}
+
+// window.open('https://daum.net');
+
+/* JS 동적 삽입 */
+// var head = document.getElementsByTagName("head")[0];
+// var script = document.createElement('script');
+// script.type = 'text/javascript';
+// script.src = ''; // src
+// head.appendChild(script);
