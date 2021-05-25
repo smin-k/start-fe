4978eb2581e58d01313313e58141bc14fedf2b77
tmdals8030 <a7965344@gmail.com>
2021-05-25 15:40:34 +0900

    kakao 수정

--- 10/kakao.js
+++ 10/kakao.js
@@ -1,14 +1,11 @@
-const $btn = document.querySelector("#search-btn");
-const $text = document.querySelector("#search-text");
-const $result = document.querySelector("#result");
-const url = "https://dapi.kakao.com/v2/search/web";
-const headers = { Authorization: "KakaoAK eddf13cbf067d7cc5dd00a72cb025072" };
+const $btn = document.querySelector('#search-btn');
+const $text = document.querySelector('#search-text');
+const $result = document.querySelector('#result');
+const url = 'https://dapi.kakao.com/v2/search/web';
+const headers = { Authorization: 'KakaoAK 70b4f5d8361f6c21662f33bd08443afa' };
 
 function success(data) {
   const { documents } = data;
-  //   const li = documnets.map((doc) => {
-  //     return `<li class="list-group-item"><a href="${doc.url}" target="_blank">${doc.contents}</a></li>`;
-  //   });
   let str = ``;
   for (let i = 0; i < documents.length; i += 1) {
     const doc = documents[i];
@@ -19,14 +16,14 @@ function success(data) {
 }
 function error() {
   // eslint-disable-next-line no-alert
-  alert("데이터를 가져올수 없습니다.");
+  alert('데이터를 가져올수 없습니다.');
 }
 
 function search() {
   const { value } = $text;
-  if (value === "") {
+  if (value === '') {
     // eslint-disable-next-line no-alert
-    alert("검색어를 입력하세요");
+    alert('검색어를 입력하세요');
     return false;
   }
   fetch(`${url}?query=${value}`, { headers })
@@ -37,4 +34,4 @@ function search() {
   return true;
 }
 
-$btn.addEventListener("click", search);
+$btn.addEventListener('click', search);
